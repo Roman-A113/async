965d5d2b2b8e05612d1723b38e6df893e364efb6
Roman <roman.koles@bk.ru>
2026-04-28 19:24:13 +0500

    done 3

--- static/focus.js
+++ static/focus.js
@@ -1,34 +1,38 @@
 const API = {
     organizationList: "/orgsList",
-    analytics: "/api3/analytics",
+    analytics: "/api3/analitics",
     orgReqs: "/api3/reqBase",
     buhForms: "/api3/buh",
 };
 
 async function run() {
-    const orgOgrns = await sendRequest(API.organizationList);
-    const ogrns = orgOgrns.join(",");
+    try {
+        const orgOgrns = await sendRequest(API.organizationList);
+        const ogrns = orgOgrns.join(",");
 
-    const requisites = await sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
-    const orgsMap = reqsToMap(requisites);
+        const requisites = await sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
+        const orgsMap = reqsToMap(requisites);
 
-    const analytics = await sendRequest(`${API.analytics}?ogrn=${ogrns}`);
-    addInOrgsMap(orgsMap, analytics, "analytics");
+        const analytics = await sendRequest(`${API.analytics}?ogrn=${ogrns}`);
+        addInOrgsMap(orgsMap, analytics, "analytics");
 
-    const buh = await sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
-    addInOrgsMap(orgsMap, buh, "buhForms");
-    render(orgsMap, orgOgrns);
+        const buh = await sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
+        addInOrgsMap(orgsMap, buh, "buhForms");
+        render(orgsMap, orgOgrns);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 run();
 
 function sendRequest(url) {
-    return fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-        });
+    return fetch(url).then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        alert(`${response.status} ${response.statusText}`);
+    });
 }
 
 function reqsToMap(requisites) {
